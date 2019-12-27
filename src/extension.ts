e3ea89c0500c143febbce789043ab973ba6e2fe7
lvxiaoxin <lvxin0405@outlook.com>
2019-12-27 17:16:44 +0800

    feature: introduced eslint

--- src/extension.ts
+++ src/extension.ts
@@ -9,33 +9,17 @@ import { toUpperCaseDisposable } from './utils/toUpperCase';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "x-processor-utils" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World!');
-	// });
-
-	// -------------------- Beginning ---------------------
-		
 	// x cmd: xReverse --> To reverse the selections.
 	context.subscriptions.push(reverseDisposable);
 
 	// x cmd: xToLowerCase --> To lowercase the selections.
 	context.subscriptions.push(toUpperCaseDisposable);
-	
+
 	// x cmd: xToUpperCase --> To uppercase the selections.
 	context.subscriptions.push(toLowerCaseDisposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void { }
